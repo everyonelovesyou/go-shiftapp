46314bf33127ff3890944102bfe3afcc0eff9599
K <90019300+everyonelovesyou@users.noreply.github.com>
2023-08-19 16:08:37 +0900

    update: HTMLを返すWebサーバを作成

--- main.go
+++ main.go
@@ -1,10 +1,25 @@
 package main
 
 import (
-	"fmt"
-	"os"
+	"html/template"
+	"net/http"
 )
 
 func main() {
-	fmt.Fprint(os.Stdout, "Hello Kitty!")
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", handler)
+
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+	server.ListenAndServe()
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	tpl := `<p>{{ . }}</p>`
+	data := "Hello Kitty!"
+	t := template.Must(template.New("hello").Parse(tpl))
+	t.Execute(w, data)
 }
