8ef2ca8953315972d691c615e5cc982fc1d86ce0
K <90019300+everyonelovesyou@users.noreply.github.com>
2023-10-28 13:50:54 +0900

    style: ファイル末尾に空行を挿入、インデントのサイズを2に

--- src/public/theme/theme.js
+++ src/public/theme/theme.js
@@ -1,16 +1,16 @@
 function setTheme(themeName) {
-    localStorage.setItem('theme', themeName);
-    document.documentElement.className = themeName;
+  localStorage.setItem('theme', themeName);
+  document.documentElement.className = themeName;
 }
 
 export function toggleTheme() {
-    localStorage.getItem('theme') == 'theme-dark'
-        ? setTheme('theme-light')
-        : setTheme('theme-dark');
+  localStorage.getItem('theme') == 'theme-dark'
+    ? setTheme('theme-light')
+    : setTheme('theme-dark');
 }
 
 export function setInitialTheme() {
-    window.matchMedia('(prefers-color-scheme: dark)').matches
-        ? setTheme('theme-dark')
-        : setTheme('theme-light');
+  window.matchMedia('(prefers-color-scheme: dark)').matches
+    ? setTheme('theme-dark')
+    : setTheme('theme-light');
 }
